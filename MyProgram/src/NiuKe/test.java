b99bb0107d4bccf080411197cf9319b8dc57a9fe
RikXiao <274146571@qq.com>
2017-07-04 17:34:04 +0800

    拉分支

--- MyProgram/src/NiuKe/test.java
+++ MyProgram/src/NiuKe/test.java
@@ -2,8 +2,11 @@ package NiuKe;
 
 public class test{
 	String nameString ;
+	String code;
 	private void pulic() {
 		// TODO Auto-generated method stub
      nameString = "新建分支";
+	code = "拉分支"
 	}
+	
 }
